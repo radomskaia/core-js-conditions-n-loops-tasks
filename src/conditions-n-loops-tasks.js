c4e7916c31514fdfc29f0957718757cadf8d14ec
Alena Radomskaia <radomskaia.a.i@gmail.com>
2024-12-03 19:53:24 +0700

    feat: add part of solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -246,8 +246,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str[i];
+  }
+  return newStr === str;
 }
 
 /**
